4c085bf4653e356e1ab9ecd097eded8efd436078
yongseongjeon <yongseong.jeon95@gmail.com>
2023-10-27 21:45:32 +0900

    feat: TailwindCSS 설치 및 적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,24 @@ import ReactMarkdown from "react-markdown";
 import { getPost } from "../utils/getPost";
 
 const Home: NextPage = ({ metadata, content }) => {
-  return <ReactMarkdown>{content}</ReactMarkdown>;
+  return (
+    <>
+      <h1>{metadata.title}</h1>
+      <div>{metadata.date}</div>
+      <div className="flex space-x-2">
+        {metadata.tags.map((tag: string) => (
+          <span key={tag} className="bg-blue-500 text-white px-2 py-1 rounded-full">
+            {tag}
+          </span>
+        ))}
+      </div>
+      <ReactMarkdown>{content}</ReactMarkdown>
+    </>
+  );
 };
 
 export async function getStaticProps() {
-  const { metadata, content } = getPost({ fileName: "2023-10-27-hello.md" });
+  const { metadata, content } = getPost({ fileName: "2023-10-26-test.md" });
 
   return {
     props: {
