23bd4a2bf508c3c20e23602e03e9d16d319a3a9f
yongseongjeon <yongseong.jeon95@gmail.com>
2023-10-30 15:00:56 +0900

    feat: Header 컴포넌트 추가

---
+++ components/Header/Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header className="flex justify-between h-10 px-4 mt-8">
+      <div>
+        <span className="text-3xl">Rumlog</span>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
