aa25e04a0fdc2c62df7f272f93fd50c0d0742339
yongseongjeon <yongseong.jeon95@gmail.com>
2023-11-02 17:40:35 +0900

    feat: Footer에 Resume 추가

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,6 +2,9 @@ function Footer() {
   return (
     <footer className="flex justify-center h-16 px-4 mt-8">
       <div className="text-xl flex gap-8">
+        <a href="https://rumka.notion.site/Yongseong-Jeon-43e8dae33e0049dfada8163051226901?pvs=4" className="h-8">
+          Resume
+        </a>
         <a href="https://github.com/yongseongjeon" className="h-8">
           GitHub
         </a>
