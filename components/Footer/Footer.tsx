c0e169d140b6400d4f0f68d07e235b4140a704e7
yongseongjeon <yongseong.jeon95@gmail.com>
2023-11-06 16:57:44 +0900

    feat: Footer 내용 변경

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,17 +1,8 @@
 function Footer() {
+  const curYear = new Date().getFullYear();
   return (
-    <footer className="flex justify-center h-16 px-4 mt-8">
-      <div className="text-xl flex gap-8">
-        <a href="https://rumka.notion.site/Yongseong-Jeon-43e8dae33e0049dfada8163051226901?pvs=4" className="h-8">
-          Resume
-        </a>
-        <a href="https://github.com/yongseongjeon" className="h-8">
-          GitHub
-        </a>
-        <a href="mailto:yongseong.jeon95@gmail.com" className="h-8">
-          Email
-        </a>
-      </div>
+    <footer className="flex justify-center h-16 p-8 text-gray-400">
+      <p>© {curYear} Rumka. All Rights Reserved.</p>
     </footer>
   );
 }
