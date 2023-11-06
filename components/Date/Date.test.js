d3ff54fc2f18f69f8fb7f9852c74a1250964250d
yongseongjeon <yongseong.jeon95@gmail.com>
2023-11-06 16:50:02 +0900

    test: Date 컴포넌트 테스트 추가

---
+++ components/Date/Date.test.js
@@ -0,0 +1,9 @@
+import Date from "./Date";
+import { render, screen } from "@testing-library/react";
+
+describe("Date", () => {
+  it("올바른 날짜 형식으로 렌더링한다.", () => {
+    render(<Date date={"2023-10-30 12:24:00"} />);
+    expect(screen.getByText("2023년 10월 30일")).toBeInTheDocument();
+  });
+});
