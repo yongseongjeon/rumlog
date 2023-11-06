34ae8114a941b88257deb25d84e6bd6df462923b
yongseongjeon <yongseong.jeon95@gmail.com>
2023-11-06 17:20:11 +0900

    feat: Header에 Resume, GitHub 추가 및 sticky 설정

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,12 +1,23 @@
 import Link from "next/link";
 
+const RESUME_LINK = "https://rumka.notion.site/Yongseong-Jeon-43e8dae33e0049dfada8163051226901?pvs=4";
+const GITHUB_LINK = "https://github.com/yongseongjeon";
+
 function Header() {
   return (
-    <header className="flex justify-between h-20 px-4 mt-8">
+    <header className="sticky top-0 flex justify-between h-10 p-6 text-3xl bg-white">
       <div>
-        <span className="text-3xl cursor-pointer">
+        <span className="cursor-pointer">
           <Link href="/">Rumlog</Link>
         </span>
+      </div>
+      <div className="flex gap-8">
+        <a href={RESUME_LINK} target="_blank" rel="noopener noreferrer">
+          <span>Resume</span>
+        </a>
+        <a href={GITHUB_LINK} target="_blank" rel="noopener noreferrer">
+          <span>GitHub</span>
+        </a>
       </div>
     </header>
   );
