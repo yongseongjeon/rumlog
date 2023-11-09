550414ceff306600ca9971bd00f024e09516ba6c
yongseongjeon <yongseong.jeon95@gmail.com>
2023-11-09 10:05:35 +0900

    refactor: children 태그로 넘기기

--- components/MarkdownRenderer/MarkdownRenderer.tsx
+++ components/MarkdownRenderer/MarkdownRenderer.tsx
@@ -7,15 +7,18 @@ const MarkdownRenderer = ({ markdown }) => {
   const components = {
     code({ node, inline, className, children, ...props }) {
       const match = /language-(\w+)/.exec(className || "");
+      const trimmedChildren = String(children).replace(/\n$/, "");
       return !inline && match ? (
-        <SyntaxHighlighter style={materialDark} language={match[1]} PreTag="div" children={String(children).replace(/\n$/, "")} {...props} />
+        <SyntaxHighlighter style={materialDark} language={match[1]} PreTag="div" {...props}>
+          {trimmedChildren}
+        </SyntaxHighlighter>
       ) : (
         <span className="bg-gray-200 px-1 py-0.5 rounded text-sm font-mono text-red-500">{children}</span>
       );
     },
   };
 
-  return <ReactMarkdown components={components} children={markdown} />;
+  return <ReactMarkdown components={components}>{markdown}</ReactMarkdown>;
 };
 
 export default MarkdownRenderer;
